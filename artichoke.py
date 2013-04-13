14c4bdc9bd59db3de0496400c4c9ffca50660d4d
Kurt <kespindler@gmail.com>
2013-04-13 11:44:33 +0200

    add logic to accept more article formats
    - title signaling the beginning of abstract doesn't have to be on
      separate line
    - first attempt at guessing the title of something
    - mark notes as METADATA NEED REVIEW (only) when we guess

--- artichoke.py
+++ artichoke.py
@@ -22,48 +22,78 @@ def pdfinfo(article_fname):
         info[fixedkey] = val
     return info
         
-def extract_from_pdftext(article_fname):
+def extract_from_pdftext(article_fname, guess=False):
     text = sub.check_output(['/usr/local/bin/pdftotext', '-enc', 'UTF-8', article_fname, '-'])
     text = text.decode('utf8')
 
-    first100lines = '\n'.join(text.splitlines()[:100])
-    doimatch = re.search('10\.[0-9]{4,}\/[^\s]*[^\s\.,]', first100lines)
-    doi = doimatch.group() if doimatch is not None else ''
+    results = {}
+    first_100_lines = '\n'.join(text.splitlines()[:100])
+    doimatch = re.search('10\.[0-9]{4,}\/[^\s]*[^\s\.,]', first_100_lines)
+    if doimatch:
+        results['doi'] = doimatch.group()
 
-    abstractstring = 'Abstract\n'
-    abstractindex = first100lines.find(abstractstring)
-    if abstractindex != -1:
-        abstractstartindex = abstractindex + len(abstractstring)
-        abstractendindex = first100lines.find('\n', abstractstartindex)
-        abstract = first100lines[abstractstartindex:abstractendindex]
-    else:
-        abstract = ''
+    abstract_re = 'abstract[:\n]'
+    match = re.search(abstract_re, first_100_lines, re.IGNORECASE) #I: ignore case
+    if match:
+        start_index = match.end()
+        end_index = first_100_lines.find('\n', start_index)
+        abstract = first_100_lines[start_index:end_index].strip()
+        results['abstract'] = abstract
     
-    return doi, abstract
+    if guess:
+        title_guess1 = text[:text.find('\n')]
+        results['title'] = title_guess1
+    return results
 
-def display_info(article_fname):
-    info = pdfinfo(article_fname)
+def update_info_with_bibtex(info, bibtex, keys, replace = False):
+    for key in keys:
+        val = gscholar._get_bib_element(bibtex, key)
+        if val and (replace or key not in info):
+            info[key] = val
+
+def display_info(fpath):
+    if not os.path.exists(fpath):
+        raise IOError('File Not Found')
+    info = pdfinfo(fpath)
     
-    doi, abstract = extract_from_pdftext(article_fname)
+    guessed_data = ['title', 'author']
+    guess = not all(guess in info for guess in guessed_data)
+    info_from_text = extract_from_pdftext(fpath, guess)
+    for k in info_from_text:
+        if k not in info:
+            info[k] = info_from_text[k]
 
-    query_string = doi if doi else info['title']
-    if query_string:
-        # query google
+    query_string = info['doi'] if 'doi' in info else (
+            info['title'] if 'title' in info else 
+            None)
+
+    bibtex = ''
+    if query_string: # query google
         try:
             text = gscholar.query(query_string, gscholar.FORMAT_BIBTEX, False)[0]
             if text:
                 lines = text.splitlines()
-                lines.insert(1,'  doi={{{}}},'.format(doi))
+                if 'doi' in info:
+                    lines.insert(1,'  doi={{{}}},'.format(info['doi']))
                 bibtex = '\n'.join(lines)
         except urllib2.URLError:
             pass
 
-    if 'bibtex' not in locals():
-        bibtex = ''
+    if bibtex:
+        update_info_with_bibtex(info, bibtex, guessed_data)
 
-    notetitle = ''.join([info['title'],' - ',info['author']])
-    notebody = '\n\n'.join([abstract, bibtex])
-    insert_into_evernote(notetitle, notebody, article_fname)
+    notetitle = ''.join([info.get('title', 'Unknown Title'),
+        ' - ', info.get('author', 'Authors Unknown')])
+    if guess:
+        notetitle += ' METADATA NEEDS REVIEW'
+
+    notebody = ''
+    if 'abstract' in info:
+        notebody += info['abstract'] + '\n\n'
+    if bibtex:
+        notebody += bibtex + '\n\n'
+
+    insert_into_evernote(notetitle, notebody, fpath)
 
 def asquote(astr):
   "Return the AppleScript equivalent of the given string."
@@ -81,17 +111,13 @@ tell application "Evernote"
   open note window with note1
 end tell""".format(*args)
   #tell note1 to append attachment file {}
+  #this should work, but it doesn't for some reason
     sub.call([u'/usr/bin/osascript', u'-e', ascript])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Take an article and insert it into Evernote')
     parser.add_argument( 'articles', nargs='+', help='articles to be inserted')
     parsed = parser.parse_args()
-    for article_fname in parsed.articles:
-        # TODO fix next line... duh
-        fname = article_fname.replace("~", "/Users/kurt")
-        machd = 'Macintosh HD'
-        if fname.startswith(machd):
-            fname = fname[len(machd):].replace(':','/')
-        display_info(fname)
+    for fpath in parsed.articles:
+        display_info(fpath)
     print('Complete!')
